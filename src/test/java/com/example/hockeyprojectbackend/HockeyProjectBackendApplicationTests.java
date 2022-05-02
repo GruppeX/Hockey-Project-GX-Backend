100482c7bfc21c3d35a4c226f4226290282c6da5
jackiejensen <Jackiehartvig@hotmail.com>
2022-05-02 11:36:15 +0200

    Added project

---
+++ src/test/java/com/example/hockeyprojectbackend/HockeyProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hockeyprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HockeyProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
