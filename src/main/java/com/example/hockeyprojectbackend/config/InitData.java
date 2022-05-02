afe41d9efbbeea25e97d895e66360574c1772179
Christoffer <christofferp97@hotmail.com>
2022-05-02 12:30:13 +0200

    Mapper, initData

---
+++ src/main/java/com/example/hockeyprojectbackend/config/InitData.java
@@ -0,0 +1,12 @@
+package com.example.hockeyprojectbackend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+
+public class InitData implements CommandLineRunner {
+
+@Autowired //repos
+
+@Override
+public void run(String... args) throws Exception {}
+}
