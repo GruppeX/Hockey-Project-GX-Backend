c00bb41f851f5c0038448d8b7f5c4d8526e14890
Christoffer <christofferp97@hotmail.com>
2022-05-05 13:37:50 +0200

    GXH-3 Please fix update

--- src/test/java/com/example/hockeyprojectbackend/controller/PlayerControllerImplTest.java
+++ src/test/java/com/example/hockeyprojectbackend/controller/PlayerControllerImplTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.Assert;
 
 import java.util.List;
@@ -20,7 +21,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@DataJpaTest
 class PlayerControllerImplTest {
 
 
@@ -65,19 +65,34 @@ class PlayerControllerImplTest {
 
   @Test
   void getAllPlayers() {
-    Player player = new Player();
-    player.setFirstName("Hans");
-    player.setLastName("Hansen");
-    player.setRole("Field");
-    player.setSelected(true);
 
     List<Player> players = playerControllerImpl.getAllPlayers();
 
-    Assertions.assertThat(players.size()).isGreaterThan(0);
+    assertTrue(players.size() != 0);
+
   }
+
 
   @Test
   void updatePlayer() {
+    int playerId = 1;
+    Player oldPlayer = playerRepository.findById(playerId).get();
+    oldPlayer.setFirstName("Joe");
+
+    ResponseEntity<String> playerUpdate =
+        playerControllerImpl.updatePlayer(playerId, oldPlayer);
+    Player playerUpdated = playerRepository.findById(playerId).get();
+
+    assertEquals(oldPlayer.getFirstName(), playerUpdated.getFirstName());
+    assertEquals(playerUpdate.getStatusCode(), HttpStatus.OK);
+
+    //Skal denne kunne skrives om til original eller lige meget???
+
+    // For at få orginal titlen tilbage igen efter testen
+//    Activity activityRestore = activityRepository.findById(activityId).get();
+//    activityRestore.setActivityTitle("goCart");
+//    Activity activityUpdate1 = activityRepository.save(activityRestore);
+//    org.assertj.core.api.Assertions.assertThat(activityUpdate1.getActivityTitle()).isEqualTo("goCart");
   }
 
   @Test
